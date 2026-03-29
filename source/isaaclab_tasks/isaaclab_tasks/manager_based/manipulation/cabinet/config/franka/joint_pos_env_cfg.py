3a150bbc450b1006dc0eaa8be0da8475642f0259
Matěj Kripner <kripnermatej@gmail.com>
2026-03-29 22:11:07 +0200

    Increase drawer ccd_iterations to 500 (use with fewer envs to fit in GPU memory)

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/cabinet/config/franka/joint_pos_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/cabinet/config/franka/joint_pos_env_cfg.py
@@ -30,7 +30,7 @@ class FrankaCabinetEnvCfg(CabinetEnvCfg):
             solver_cfg=MJWarpSolverCfg(
                 njmax=150,
                 nconmax=150,
-                ccd_iterations=200,
+                ccd_iterations=500,
                 ls_iterations=20,
                 cone="pyramidal",
                 impratio=1,
